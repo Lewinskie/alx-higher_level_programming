30e1b43b6d776ffa5656e978a6080a32c43f7801
Lewinskie <wangaralewis@gmail.com>
2023-10-03 04:11:12 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(letter) for letter in range(97, 123) if chr(letter) not in ['q','e']))
+print("".join(chr(letter) for letter in range(97, 123) if chr(letter) not in ['q','e']).format())
