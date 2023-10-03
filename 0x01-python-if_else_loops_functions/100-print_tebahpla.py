32d6ff90c2bb1f3888b7b8bed8140036436ae4b6
Lewinskie <wangaralewis@gmail.com>
2023-10-03 04:37:19 -0700

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letter in range(ord('z'), ord('a') -1, -1):
-    print("{:c}".format(letter if letter % 2 == 1 else letter - 32), end="")
+    print("{:c}".format(letter if ord('z') - letter % 4 == 0 else letter - 32), end="")
