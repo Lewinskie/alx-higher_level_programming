4497539dfe5b0d67d2f22a1a2a8b3f24c85c9cd8
Lewinskie <wangaralewis@gmail.com>
2023-10-09 02:30:22 -0700

    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+    new_list = []
+    for i in range(len(my_list)):
+        if i == idx:
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+    return new_list
